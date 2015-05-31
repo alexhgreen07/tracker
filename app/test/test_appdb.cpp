f7d6eade8222e500b8ee2e5d81be628e61fd5f31
Alex Green <alex.h.green07@gmail.com>
2015-05-31 18:49:37 -0400

    Added skeleton for application database

---
+++ app/test/test_appdb.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+#include <CppUTest/TestHarness.h>
+
+using namespace Tracker::Application;
+
+TEST_GROUP(AppDBGroup)
+{
+};
+
+TEST(AppDBGroup, BasicInitialize)
+{
+    //TODO: implement basic test
+}
