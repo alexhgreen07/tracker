feb666a5645b7eb671e894f490b81e73039677d3
Alex Green <alex.h.green07@gmail.com>
2015-06-01 22:53:00 -0400

    Added basic skeleton for application database.

--- app/test/test_appdb.cpp
+++ app/test/test_appdb.cpp
@@ -1,13 +1,45 @@
+#include <database.hpp>
+
+namespace Tracker
+{
+namespace Application
+{
+
+using namespace Database;
+    
+class AppDB : public DatabaseSqlite3
+{
+public:
+    AppDB()
+    {}
+};
+
+}
+}
+
 #include <string>
 #include <iostream>
 
 #include <CppUTest/TestHarness.h>
 
+using namespace Tracker::Application;
+
 TEST_GROUP(AppDBGroup)
 {
+    AppDB testDB;
+    
+    TEST_SETUP()
+    {
+        testDB.open(":memory:");
+    }
+    
+    TEST_TEARDOWN()
+    {
+        testDB.close();
+    }
 };
 
-TEST(AppDBGroup, BasicInitialize)
+TEST(AppDBGroup, BasicOpen)
 {
-    //TODO: implement basic test
+    CHECK(testDB.isConnected());
 }
