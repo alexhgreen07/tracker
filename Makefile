78017c5645fb81e38a0b6b707815b480fde1e1cd
Alex Green <alex.h.green07@gmail.com>
2015-06-08 22:26:29 -0400

    Added basic library build

--- Makefile
+++ Makefile
@@ -2,8 +2,20 @@ CC = g++
 CFLAGS = -std=c++11
 BIN_DIR = bin
 
-all: core.a
-clean:
-	rm -rf $(BIN_DIR)
+include_dirs = 
 
 include core/core.mk
+
+$(BIN_DIR)/%.o: %.cpp
+	mkdir -p $(dir $@)
+	$(CC) -c -o $@ $< -I $(include_dirs) $(CFLAGS)
+
+$(BIN_DIR)/%.a:
+	ar rcs $@ $^
+
+.PHONY: all
+all: core_lib.a
+
+.PHONY: clean
+clean:
+	rm -rf bin/
--- core/core.mk
+++ core/core.mk
@@ -1,17 +1,11 @@
-core_include_dirs = \
+include_dirs += \
 	core/include
 
 core_source_files = \
 	core/src/event.o \
 	core/src/scheduler.o \
 	core/src/task.o
-core_source_objects = $(addprefix $(BIN_DIR)/,$(core_source_files:.cpp=.o))
 
-core.a: $(BIN_DIR)/core.a
+core_obj_files = $(patsubst %,$(BIN_DIR)/%,$(core_source_files))
 
-$(BIN_DIR)/core.a: $(core_source_objects)
-	ar -cvq $@ $(core_source_objects)
-
-$(core_source_objects): $(BIN_DIR)/%.o: %.cpp
-	mkdir -p $(dir $@)
-	$(CC) $(CFLAGS) -I $(core_include_dirs) -c $< -o $@
+$(BIN_DIR)/core_lib.a: $(core_obj_files)
