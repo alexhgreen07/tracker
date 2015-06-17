056c0a0bac866e85c2d0b506572205d5acfa2db9
Alex Green <alex.h.green07@gmail.com>
2015-06-16 20:56:53 -0400

    Added dummy test to validate say hello

--- app/test/test_app_api.cpp
+++ app/test/test_app_api.cpp
@@ -3,8 +3,15 @@
 
 #include <CppUTest/TestHarness.h>
 
+#include "app_api.hpp"
+
+using namespace Tracker;
+using namespace Application;
+
 TEST_BASE(AppApiGroupBase)
 {
+	AppApi testApi;
+	
 	AppApiGroupBase()
 	{}
 };
@@ -21,6 +28,15 @@ TEST_GROUP_BASE(AppApiGroup, AppApiGroupBase)
 	}
 };
 
-TEST(AppApiGroup, Basic)
+TEST(AppApiGroup, ValidateSayHello)
 {
+	auto procedures = testApi.getProcedures();
+	Json::Value params;
+	Json::Value results;
+	
+	params["name"] = "test name";
+	procedures["sayHello"]->call(params,results);
+	
+	Json::Value desiredResult = "Hello: " + params["name"].asString();
+	CHECK(results == desiredResult);
 }
