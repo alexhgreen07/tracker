12a84cc0806ccf00eac61315fb44a74e2671b682
Alex Green <alex.h.green07@gmail.com>
2015-06-22 19:48:56 -0400

    Added require.js and updated jquery

---
+++ ui/app.js
@@ -0,0 +1,14 @@
+// For any third party dependencies, like jQuery, place them in the lib folder.
+
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+	paths: {
+		jquery: 'externals/jquery-2.1.4.min'
+	}
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['main']);
--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,31 @@
-idx = 1;
+define(['jquery'],function($){
 
-function doAjaxRequest(m, parameters, target) {
-	var tmpObject = {jsonrpc: 2.0, method: m, params: parameters, id: idx++};
-	var string = JSON.stringify(tmpObject);
-	
-	$.ajax({
-		type: "POST",
-		url: ".",
-		data: string,
-		dataType: "json",
-	   contentType: "application/json",
-	})
-	.done(function(data) {
-		$(target).html(data.result);
-	})
-	.fail(function() {
-		alert("Ajax failed to fetch data");
-	})
-}
-
-$(document).ready(function(){
-	$("#testform").submit(function() {
-	  doAjaxRequest('sayHello',{name:$("#thename").val()},"#result");
-	  return false;
+   idx = 1;
+   
+   function doAjaxRequest(m, parameters, target) {
+   var tmpObject = {jsonrpc: 2.0, method: m, params: parameters, id: idx++};
+   var string = JSON.stringify(tmpObject);
+   
+   $.ajax({
+		  type: "POST",
+		  url: ".",
+		  data: string,
+		  dataType: "json",
+		  contentType: "application/json",
+		  })
+   .done(function(data) {
+		 $(target).html(data.result);
+		 })
+   .fail(function() {
+		 alert("Ajax failed to fetch data");
+		 })
+   }
+   
+	$(document).ready(function(){
+		$("#testform").submit(function() {
+		   doAjaxRequest('sayHello',{name:$("#thename").val()},"#result");
+		   return false;
+	   });
 	});
+	   
 });
