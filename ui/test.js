884e53a53826fbe2af4670273ab98fa35dc1d36a
Alex Green <alex.h.green07@gmail.com>
2015-06-22 20:34:24 -0400

    Added jasmine and skeleton test

---
+++ ui/test.js
@@ -0,0 +1,29 @@
+// Requirejs Configuration Options
+require.config({
+	// paths: maps ids with paths (no extension)
+	paths: {
+		'jasmine': ['externals/jasmine-2.3.4/jasmine'],
+		'jasmine-html': ['externals/jasmine-2.3.4/jasmine-html'],
+		'jasmine-boot': ['externals/jasmine-2.3.4/boot']
+	},
+	// shim: makes external libraries compatible with requirejs (AMD)
+	shim: {
+		'jasmine-html': {
+			deps : ['jasmine']
+		},
+		'jasmine-boot': {
+			deps : ['jasmine', 'jasmine-html']
+		}
+	}
+});
+
+specs = [
+	"test/test_spec.js"
+];
+
+require(['jasmine-boot'], function () {
+	require(specs, function(){
+		//trigger Jasmine
+		window.onload();
+	})
+});
---
+++ ui/test/test_spec.js
@@ -0,0 +1,10 @@
+define([],function(){
+
+	describe("test", function(){
+		describe("sayHello", function(){
+			it("should say Hello", function(){
+				expect("Hello").toEqual("Hello");
+			});
+		});
+	});
+});
