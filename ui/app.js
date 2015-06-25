b85113108b0f94e375a627466f0396287a4e771f
Alex Green <alex.h.green07@gmail.com>
2015-06-24 22:17:42 -0400

    General cleanup to test html

--- ui/app.js
+++ ui/app.js
@@ -15,6 +15,9 @@ requirejs.config( {
 	}
 });
 
-// Start loading the main app file. Put all of
-// your application logic in there.
-requirejs( [ 'main' ]);
+if(!isTest)
+{
+	// Start loading the main app file. Put all of
+	// your application logic in there.
+	requirejs( [ 'main' ]);
+}
