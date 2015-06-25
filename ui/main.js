6d371ab373640165d1a78713f82a99de1f76f3ff
Alex Green <alex.h.green07@gmail.com>
2015-06-24 22:07:48 -0400

    Re-format and cleanup

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,31 @@
-define(['jquery','jqueryjsonrpc'],function($){
+define( [ 'jquery', 'jqueryjsonrpc' ], function($) {
 
-   idx = 1;
-   
-   function doAjaxRequest(m, parameters, target) {
-	   var tmpObject = {jsonrpc: 2.0, method: m, params: parameters, id: idx++};
-	   var string = JSON.stringify(tmpObject);
-	   
-	   $.jsonRPC.setup({
-		   endPoint: '/',
-		   namespace: ''
-	   });
-	   
+	function doAjaxRequest(m, parameters, target) {
+
 		$.jsonRPC.request(m, {
-			params: parameters,
-			success: function(data) {
+			params : parameters,
+			success : function(data) {
 				$(target).html(data.result);
 			},
-			error: function(result) {
+			error : function(result) {
 				alert("Ajax failed to fetch data");
 			}
-		 });
-   }
-   
-	$(document).ready(function(){
+		});
+	}
+
+	$(document).ready(function() {
+
+		$.jsonRPC.setup( {
+			endPoint : '/',
+			namespace : ''
+		});
+
 		$("#testform").submit(function() {
-		   doAjaxRequest('sayHello',{name:$("#thename").val()},"#result");
-		   return false;
-	   });
+			doAjaxRequest('sayHello', {
+				name : $("#thename").val()
+			}, "#result");
+			return false;
+		});
 	});
-	   
+
 });
