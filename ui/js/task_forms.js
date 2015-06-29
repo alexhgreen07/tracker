fb46fa9f81da5b62659277d91684a7842717a8fe
Alex Green <alex.h.green07@gmail.com>
2015-06-29 00:12:37 -0400

    Added insert task on button click

--- ui/js/task_forms.js
+++ ui/js/task_forms.js
@@ -1,13 +1,33 @@
-define( [ 'moment', 'jquery', 'jqueryui' ], function(moment,$) {
+define( [ 'moment', 'jquery', 'jqueryui', 'datetimepicker' ], function(moment,$) {
 	
-	function AddTaskForm()
+	function AddTaskForm(api)
 	{
+		this.api = api;
+		
 		this.earliestStartTimeInput = null;
 		this.latestEndTimeInput = null;
 		this.durationInput = null;
+		this.resultsDiv = null;
 		
 		this.submitButton = null;
 	}
+	AddTaskForm.prototype.submitSuccess = function()
+	{
+		this.resultsDiv.innerHTML = "Task submitted";
+	};
+	AddTaskForm.prototype.submitError = function()
+	{
+		this.resultsDiv.innerHTML = "Task submission error";
+	};
+	AddTaskForm.prototype.submitClickEvent = function()
+	{
+		this.api.insertTask(
+				$(this.earliestStartTimeInput).datetimepicker('getDate').getTime(),
+				$(this.latestEndTimeInput).datetimepicker('getDate').getTime(),
+				parseInt(this.durationInput.value),
+				this.submitSuccess.bind(this),
+				this.submitError.bind(this));
+	};
 	AddTaskForm.prototype.render = function(parent)
 	{
 		var div = parent.appendChild(document.createElement("div"));
@@ -32,6 +52,12 @@ define( [ 'moment', 'jquery', 'jqueryui' ], function(moment,$) {
 		div.appendChild(document.createElement("br"));
 		this.durationInput = div.appendChild(document.createElement("input"));
 		this.durationInput.type = "text";
+		this.durationInput.value = 0;
+		
+		div.appendChild(document.createElement("br"));
+		div.appendChild(document.createElement("br"));
+		
+		this.resultsDiv = div.appendChild(document.createElement("div"));
 		
 		div.appendChild(document.createElement("br"));
 		div.appendChild(document.createElement("br"));
@@ -41,8 +67,11 @@ define( [ 'moment', 'jquery', 'jqueryui' ], function(moment,$) {
 		this.submitButton.value = "Submit";
 		
 		div.appendChild(document.createElement("br"));
-		
+
+		$(this.earliestStartTimeInput).datetimepicker();
+		$(this.latestEndTimeInput).datetimepicker();
 		$(this.submitButton).button();
+		$(this.submitButton).click(this.submitClickEvent.bind(this));
 	};
 	
 	return {
--- ui/test/test_task_forms.js
+++ ui/test/test_task_forms.js
@@ -4,9 +4,31 @@ define( [ 'js/task_forms' ], function(libTaskForms) {
 		
 		var testDiv = document.getElementById("test_div");
 		var testForm = null;
+		var testApi = null;
+		
+		function DummyApi()
+		{
+			this.lastAddedTask = null;
+		}
+		DummyApi.prototype.insertTask = function(earliestStartTime,latestEndTime,duration,success,error)
+		{
+			this.lastAddedTask = {
+				earliestStartTime: earliestStartTime,
+				latestEndTime: latestEndTime,
+				duration: duration
+			};
+			
+			success(true);
+		};
+		
+		function roundTimeToMinute(time)
+		{
+			return Math.floor(time.getTime() / 60 / 1000) * 60 * 1000;
+		}
 		
 		beforeEach(function() {
-			testForm = new libTaskForms.AddTaskForm();
+			testApi = new DummyApi();
+			testForm = new libTaskForms.AddTaskForm(testApi);
 		});
 		
 		it("is allocated", function() {
@@ -16,11 +38,30 @@ define( [ 'js/task_forms' ], function(libTaskForms) {
 		it("renders form", function() {
 			testForm.render(testDiv);
 			expect(testDiv.innerHTML).not.toBe("");
-			alert("");
+		});
+		
+		it("adds a task on a button click", function() {
+			
+			testForm.render(testDiv);
+			
+			var now = new Date();
+			
+			$(testForm.earliestStartTimeInput).datetimepicker('setDate', now );
+			$(testForm.latestEndTimeInput).datetimepicker('setDate', now );
+			testForm.durationInput.value = 0;
+			
+			testForm.submitButton.click();
+			
+			//round to minute
+			var expectedTime = roundTimeToMinute(now);
+			expect(testApi.lastAddedTask.earliestStartTime).toEqual(expectedTime);
+			expect(testApi.lastAddedTask.latestEndTime).toEqual(expectedTime);
+			expect(testApi.lastAddedTask.duration).toEqual(0);
 		});
 		
 		afterEach(function() {
 			testForm = null;
+			testApi = null;
 			testDiv.innerHTML = "";
 		});
 		
