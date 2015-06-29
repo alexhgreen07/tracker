22ba2a8dad87b51806af65c7ca601d77507cef9e
Alex Green <alex.h.green07@gmail.com>
2015-06-28 23:30:32 -0400

    Added add task form render

--- ui/js/task_forms.js
+++ ui/js/task_forms.js
@@ -2,8 +2,48 @@ define( [ 'moment', 'jquery', 'jqueryui' ], function(moment,$) {
 	
 	function AddTaskForm()
 	{
+		this.earliestStartTimeInput = null;
+		this.latestEndTimeInput = null;
+		this.durationInput = null;
 		
+		this.submitButton = null;
 	}
+	AddTaskForm.prototype.render = function(parent)
+	{
+		var div = parent.appendChild(document.createElement("div"));
+		
+		div.appendChild(document.createTextNode("Earliest Start Time"));
+		div.appendChild(document.createElement("br"));
+		this.earliestStartTimeInput = div.appendChild(document.createElement("input"));
+		this.earliestStartTimeInput.type = "text";
+		
+		div.appendChild(document.createElement("br"));
+		div.appendChild(document.createElement("br"));
+		
+		div.appendChild(document.createTextNode("Latest End Time"));
+		div.appendChild(document.createElement("br"));
+		this.latestEndTimeInput = div.appendChild(document.createElement("input"));
+		this.latestEndTimeInput.type = "text";
+		
+		div.appendChild(document.createElement("br"));
+		div.appendChild(document.createElement("br"));
+		
+		div.appendChild(document.createTextNode("Duration"));
+		div.appendChild(document.createElement("br"));
+		this.durationInput = div.appendChild(document.createElement("input"));
+		this.durationInput.type = "text";
+		
+		div.appendChild(document.createElement("br"));
+		div.appendChild(document.createElement("br"));
+		
+		this.submitButton = div.appendChild(document.createElement("input"));
+		this.submitButton.type = "submit";
+		this.submitButton.value = "Submit";
+		
+		div.appendChild(document.createElement("br"));
+		
+		$(this.submitButton).button();
+	};
 	
 	return {
 		AddTaskForm: AddTaskForm
--- ui/test/test_task_forms.js
+++ ui/test/test_task_forms.js
@@ -2,6 +2,7 @@ define( [ 'js/task_forms' ], function(libTaskForms) {
 	
 	describe("TaskFormsLib Add Task Suite", function() {
 		
+		var testDiv = document.getElementById("test_div");
 		var testForm = null;
 		
 		beforeEach(function() {
@@ -12,8 +13,15 @@ define( [ 'js/task_forms' ], function(libTaskForms) {
 			expect(testForm).not.toEqual(null);
 		});
 		
+		it("renders form", function() {
+			testForm.render(testDiv);
+			expect(testDiv.innerHTML).not.toBe("");
+			alert("");
+		});
+		
 		afterEach(function() {
 			testForm = null;
+			testDiv.innerHTML = "";
 		});
 		
 	});
