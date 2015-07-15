729fd20b47cccf628c4729e7f112622697f390e1
Alex Green <alex.h.green07@gmail.com>
2015-07-14 20:59:32 -0400

    Added tests for text input

--- ui/test/test_form_helpers.js
+++ ui/test/test_form_helpers.js
@@ -1,14 +1,42 @@
 define( [ 'js/form_helpers' ], function(libFormHelpers) {
 	
-	describe("TaskFormsLib Add Task Suite", function() {
+	var testDiv = document.getElementById("test_div");
+	
+	describe("Form Helpers Text Input Suite", function() {
 		
-		var testDiv = document.getElementById("test_div");
+		var testInput = null;
+		var testName = "Name";
+		var defaultValue = "Value";
 		
 		beforeEach(function() {
-			
+			testInput = new libFormHelpers.TextFormField(testName,defaultValue);
+			testInput.render(testDiv);
+		});
+		
+		it("is allocated", function() {
+			expect(testInput).not.toEqual(null);
+		});
+		
+		it("renders the input", function() {
+			expect(testDiv.innerHTML).toContain(testName);
+		});
+		
+		it("is default value", function() {
+			expect(testInput.getValue()).toEqual(defaultValue);
+		});
+		
+		it("sets value", function() {
+			var newValue = "New Name";
+			testInput.setValue(newValue);
+			expect(testInput.getValue()).toEqual(newValue);
+		});
+		
+		it("validates input HTML", function() {
+			expect(testInput.textInput.value).toEqual(defaultValue);
 		});
 		
 		afterEach(function() {
+			testInput = null;
 			testDiv.innerHTML = "";
 		});
 		
